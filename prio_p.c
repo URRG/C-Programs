ee9965d4c901e69fa1dc5441812a27ca62c91819
Utkarsh Rastogi <95937956+URRG@users.noreply.github.com>
2023-03-10 22:12:00 +0530

    Update prio_p.c

--- prio_p.c
+++ prio_p.c
@@ -24,4 +24,44 @@ int main()
     
     a[9].PT=10000;
     
-   
+    for(t=0;count!=n;t++)
+    {
+        short_p=9;
+        for(int i=0;i<n;i++)
+        {
+            if(a[short_p].PT>a[i].PT && a[i].AT<=t && a[i].BT>0)
+            {
+                short_p=i;
+            }
+        }
+        
+        a[short_p].BT=a[short_p].BT-1;
+        
+        if(a[short_p].BT==0)
+        {
+ 
+            count++;
+            a[short_p].WT=t+1-a[short_p].AT-temp[short_p];
+            a[short_p].TAT=t+1-a[short_p].AT;
+            
+            total_WT=total_WT+a[short_p].WT;
+            total_TAT=total_TAT+a[short_p].TAT;
+            
+        }
+    }
+    
+    Avg_WT=total_WT/n;
+    Avg_TAT=total_TAT/n;
+    
+    printf("ID WT TAT\n");
+    for(int i=0;i<n;i++)
+    {
+        printf("%d %d\t%d\n",i+1,a[i].WT,a[i].TAT);
+    }
+    
+    printf("Avg waiting time of the process  is %f\n",Avg_WT);
+    printf("Avg turn around time of the process is %f\n",Avg_TAT);
+    
+    return 0;
+}
+

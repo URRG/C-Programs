464dbf48305df5ccd7f3d3c13bb18b30266eb2fa
Utkarsh Rastogi <95937956+URRG@users.noreply.github.com>
2023-04-21 23:20:37 +0530

    Update student_struct.c

--- student_struct.c
+++ student_struct.c
@@ -12,24 +12,4 @@ struct detail
 
 int main()
 {
-    struct detail s;
-    printf("Enter Name");
-    scanf("%c",&s.name);
-    printf("Enter SAPID");
-    scanf("%d",&s.SAPID);
-    printf("Enter birth day");
-    scanf("%d",&birthday.day);
-    printf("Enter birth month");
-    scanf("%c",&birthday.month);
-    printf("Enter birth year");
-    scanf("%c",&birthday.year);
-    printf("Enter Joining day");
-    scanf("%d",&join.day);
-    printf("Enter joining month");
-    scanf("%c",&join.month);
-    printf("Enter joining year");
-    scanf("%c",&join.year);
-
-    return 0;
-}
-
+   
